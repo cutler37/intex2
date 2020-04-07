e1eeb8c0699ccbb10a4e36bec30d87c52049363d
David <david.cutler@byu.edu>
2020-04-07 11:13:48 -0600

    first committ

--- intex/api/views.py
+++ intex/api/views.py
@@ -1,33 +1,38 @@
 from django.shortcuts import render
 from api.serializers import CampaignSerializer
 from api.models import Campaign,CurrencyCode,Category
-
+from django.core.serializers import serialize
 from django.views.decorators.csrf import csrf_exempt
-
+from django.core.serializers.json import DjangoJSONEncoder
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import JsonResponse
+import json
+
+class LazyEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Campaign):
+            return str(obj)
+        return super().default(obj)
 # Create your views here.
 class CampaignList (APIView):
     @csrf_exempt
     def get(self, request, format = None):
-        campaigns = Campaign.objects.all()
-        serializer = CampaignSerializer(campaigns)
-        print(serializer)
-        return Response(serializer.data)
+        otherway = serialize("json", Campaign.objects.all(),cls=LazyEncoder)
+        return Response(json.loads(otherway))
 
 class SearchCampaign (APIView):
     @csrf_exempt
     def get(self,request,campaignID,format=None):
         print(campaignID)
-        campaigns = Campaign.objects.get(campaign_id= campaignID)
-        serializer = CampaignSerializer(campaigns)
-        return Response(serializer.data)
+        otherway = serialize("json", Campaign.objects.filter(campaign_id= campaignID),cls=LazyEncoder)
+        return Response(json.loads(otherway))
 
 class SearchCampaignTitle (APIView):
     @csrf_exempt
     def get(self,request,titles,format=None):
         print(titles)
         campaigns = Campaign.objects.filter(title= titles)
-        serializer = CampaignSerializer(campaigns)
-        return Response(serializer.data)
+        otherway = serialize("json", campaigns,cls=LazyEncoder)
+        return Response(json.loads(otherway))
